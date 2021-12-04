f3ad2fb0433303901e0870ae04460b8663a0f666
shadeyg-alt <90427214+shadeyg-alt@users.noreply.github.com>
2021-12-04 15:53:44 -0600

    so much stuff (#8)
    * AI stuff (not working)
    
    * smarter AI
    
    * fix some scoring issues
    
    * almost have double jumping
    
    * fix multiple jumps
    
    * fix king bug when taking multiple jumps
    
    * fix king bug and AI can double jump
    
    * many bug fixes regarding computer
    
    * select between game type
    
    * better input validation
    
    * fix bug with forced doubles and win checking
    
    * reset board and clear
    
    * remove debug prints

--- AI.c
+++ AI.c
@@ -5,15 +5,17 @@
 
 #define ISKING(p) (p == 'O')
 
-size_t moves = 1;
+size_t moves;
 Move *availableMoves = NULL;
 
 int validMoves(char board[][8]){
     int i, j, k, l;
     int iter = 0;
+    moves = 1;
     availableMoves = (Move*) malloc(sizeof(Move));
     Move *currentMove = NULL;
     int checks = 2;
+    int hasJump = 1;
     for (j = 0; j < 8; j++){
         for (i = 0; i < 8; i++){;
             iter++;
@@ -24,6 +26,7 @@ int validMoves(char board[][8]){
             for (int x = 0; x < checks; x++){
                 currentMove = &availableMoves[moves-1];
                 currentMove->jump = 0;
+                currentMove->score = 0;
                 switch (x){
                     case 0:
                         k = i + 1;
@@ -57,6 +60,7 @@ int validMoves(char board[][8]){
                     if (tolower(board[l][k]) == 'x'){
                         if (canJump(board, currentMove) == 1){
                             currentMove->jump = 1;
+                            currentMove->score += 2;
                         }
                         else{
                             continue;
@@ -66,7 +70,24 @@ int validMoves(char board[][8]){
                         continue;
                     }
                 }
-                printf("%d, %d to %d, %d is a valid move\n", i, j, k, l);
+                if (hasNeighbors(board, currentMove, 'o') == 1){
+                    currentMove->score++;
+                }
+                if (hasNeighbors(board, currentMove, 'x') == 1){
+                    if (currentMove->jump == 1){
+                        if (canJump(board, currentMove) == 1){
+                            printf("can double jump\n");
+                            currentMove->score += 2;
+                        }
+                        currentMove->k = currentMove->jumpX;
+                        currentMove->l = currentMove->jumpY;
+                        printf("can't double jump\n");
+                    }
+                    else{
+                        currentMove->score--;
+                    }
+                }
+                printf("%d, %d to %d, %d is a valid move with a score of %d\n", i, j, k, l, currentMove->score);
                 moves++;
                 availableMoves = realloc(availableMoves, moves * sizeof(Move));
             }
@@ -78,6 +99,8 @@ int validMoves(char board[][8]){
 
 
 int canJump(char board[][8], Move *move){
+    // does NOT check if a piece can jump, but checks that a piece COULD be jumped.
+    // don't get confused
     int i = move->i;
     int j = move->j;
     int k = move->k;
@@ -103,6 +126,7 @@ int canJump(char board[][8], Move *move){
     }
     char jumpedPiece = board[jumpY][jumpX];
     if (jumpedPiece == ' '){
+
         move->k = jumpX;
         move->l = jumpY;
         return 1;
@@ -110,15 +134,56 @@ int canJump(char board[][8], Move *move){
     return 0;
 }
 
+int hasNeighbors(char board[][8], Move *move, char type){
+    int x = move->k;
+    int y = move->l;
+    int jumpX = move->jumpX;
+    int jumpY = move->jumpY;
+    switch (type){
+        case 'x':
+        // && (y+1 != jumpY && x-1 != jumpX)
+            move->jumpX = x;
+            move->jumpY = y;
+            if (board[y+1][x-1] == 'x' ){
+                if (move->jump == 1){
+                    move->k = x-1;
+                    move->l = y+1;
+                }
+                return 1;
+            }
+            //  && (y+1 != jumpY && x+1 != jumpX)
+            if (board[y+1][x+1] == 'x'){
+                if (move->jump == 1){
+                    move->k = x+1;
+                    move->l = y+1;
+                }
+                return 1;
+            }
+            break;
+        case 'o':
+            if (board[y-1][x-1] == 'o' || board[y-1][x+1] == 'o'){
+                return 1;
+            }
+    }
+    return 0;
+    
+}
+
 Move pickMove(){
     Move currentMove;
+    Move bestMove;
+    bestMove.score = -2;
     for (int i = 0; i < moves - 1; i++){
         currentMove = availableMoves[i];
-        if (currentMove.jump == 1){
-            free(availableMoves);
-            return currentMove;
+        if (currentMove.score > bestMove.score){
+            bestMove = currentMove;
         }
     }
+    if (bestMove.score == -2){
+        bestMove = currentMove;
+    }
+    
+    printf("taking move with score %d %d, %d to %d, %d\n", bestMove.score, bestMove.i, bestMove.j, bestMove.k, bestMove.l);
     free(availableMoves);
-    return currentMove;
+    return bestMove;
 }
--- AI.h
+++ AI.h
@@ -7,10 +7,14 @@ typedef struct{
     int k;
     int l;
     int jump;
+    int jumpX;
+    int jumpY;
+    int score;
 } Move;
 
 int validMoves(char board[][8]);
 int canJump(char board[][8], Move *move);
+int hasNeighbors(char board[][8], Move *move, char type);
 Move pickMove();
 
 #endif
